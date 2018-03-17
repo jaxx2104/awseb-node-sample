12db5d7c8affd64b1a025202c14b334055e0b0ea
jaxx2104 <jaxx2104@gmail.com>
2018-03-18 02:20:50 +0900

    fix unix domain socket

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import express from 'express'
+import fs from 'fs'
 import log from '~/src/log'
 
+const env = process.env.NODE_ENV || 'development'
 const port = process.env.PORT || 3000
+const socket = process.env.SOCKET || '/var/run/app/node.sock'
+
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello')
 })
 
-app.listen(port, () => {
-  log('Example app listening on port 3000!')
-})
+if (env === 'development') {
+  app.listen(port, (e) => {
+    if (e) throw log(e.message)
+    log(`Example app listening on port ${port}`)
+  })
+} else {
+  try {
+    fs.unlinkSync(socket)
+  } catch (e) {
+    log(e.message)
+  }
+  app.listen(socket, (e) => {
+    if (e) throw log(e.message)
+    fs.chmodSync(socket, '660')
+    log(`Example app listening on socket ${socket}`)
+  })
+}
